d650fb0ab8010271619e8538b5ff9f05fd0d5873
Ruby-dev911 <roxannem.developer@outlook.com>
2025-12-27 18:42:09 -0600

    Update console messages for computer answers

--- javascript.js
+++ javascript.js
@@ -3,19 +3,31 @@ let userChoice = "";
 let computerAnswer = Math.floor(Math.random());
 
 if (computerAnswer === 1) {
-    console.log('I can\'t see clearly');
+    console.log('Remember every choice you make has romantic consequences.');
 } else if (computerAnswer === 2) {
-    console.log('I think it is possible');
+    console.log('Yes.');
 } else if (computerAnswer === 3) {
-    console.log('No, No, No, NO!');
+    console.log('You will be happy... You have no choice.');
 } else if (computerAnswer === 4) {
-    console.log('I see it in the future');
+    console.log('I see it in the future. You CAN\'T ESCAPE IT!');
 } else if (computerAnswer === 5) {
-    console.log('No!');
+    console.log('No! You will be okay. This is a threat.');
 } else if (computerAnswer === 6) {
-    console.log('How dare you ask me that!?');
+    console.log('You will succeed. It is inevitable.');
 } else if (computerAnswer === 7) {
-    console.log('The ancient one wills it so..');
+    console.log('If the ancient one wills it so..');
 } else if (computerAnswer === 8) {
-    console.log('You will be happy. You have no choice.');
-}
+    console.log('Happiness is coming for you. Wether you like it or not.');
+} else if (computerAnswer === 9) {
+    console.log('Try again.');
+} else if (computerAnswer === 10) {
+    console.log('You will exceed your efforts.. IT HAS BEEN WRITTEN.');
+} else if (computerAnswer === 11) {
+    console.log('Never. You will get what you need. No more no less.');
+} else if (computerAnswer === 12) {    
+    console.log('Whatever you decide. You will pay the price. THE COUNCIL HAS SPOKEN!');
+} else if (computerAnswer === 13) {
+    console.log('No. I will now allow you 10 seconds to express your dissappointment by screaming into the void.');
+} else if (computerAnswer === 14) {
+    console.log('It is the end.');
+} 
